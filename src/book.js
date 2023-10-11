d323b067e6fedd726ba0614e9ec4559f91214939
Jeremy Pearson <jeremy.pearson08@gmail.com>
2023-10-11 13:50:33 -0600

    Initial commit

--- src/book.js
+++ src/book.js
@@ -1,10 +1,21 @@
-function createTitle() {
-  
+function createTitle(title) {
+  title = "The " + title;
+  return title;
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  };
+  return mainCharacter;
+}
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,
