a6b919687d4eb8e5eb70561f8aa1621ae94d7f3c
Jeremy Pearson <jeremy.pearson08@gmail.com>
2023-10-15 15:07:07 -0600

    Added functions for book test

--- src/book.js
+++ src/book.js
@@ -12,12 +12,47 @@ function buildMainCharacter(name, age, pronouns) {
   return mainCharacter;
 }
 
+function saveReview(review, reviews) {
+  var isDuplicate = false;
+  for (var i = 0; i < reviews.length; i++) {
+    if (review === reviews[i]) {
+      isDuplicate = true;
+    }
+  }
+  if (isDuplicate) {
+  } else {
+    reviews.push(review);
+  }
+  return reviews;
+}
+
+function calculatePageCount(bookTitle) {
+  var pageCount = bookTitle.length * 20
+  return pageCount
+}
+
+function writeBook(title, mainCharacter, genre) {
+  var pageCount = calculatePageCount(title); 
+  var book = {
+    title: title,
+    mainCharacter: mainCharacter,
+    genre: genre,
+    pageCount: pageCount,
+  };
+  return book;
+}
+
+function editBook(title) {
+  var originalPageCount = title.pageCount;
+  title.pageCount = originalPageCount * 0.75;
+  return title;
+}
 
 module.exports = {
   createTitle,
   buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
